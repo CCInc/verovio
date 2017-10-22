fe5dc1ed831238c61b367634b11db3e024f25cac
Christopher Lee <coolccinc@gmail.com>
2017-10-21 22:42:19 -0500

    Added production build sequence to create verovio.min.js minimized version

--- tools/Emscripten/build.bat
+++ tools/Emscripten/build.bat
@@ -5,4 +5,5 @@
 call D:\Data\em\emsdk_env.bat
 cd D:\Projects\music\Verovio\verovio\emscripten
 perl buildToolkit -DHPX
+start D:\Projects\music\Verovio\verovio\tools\Emscripten\buildProd.bat
 pause
---
+++ tools/Emscripten/buildProd.bat
@@ -0,0 +1,4 @@
+call D:\Data\em\emsdk_env.bat
+cd D:\Projects\music\Verovio\verovio\emscripten
+perl buildToolkit -DHPXo
+pause
